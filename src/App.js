e7529123ef7a620245bdff78e9f49168c897146b
nbvishal@noborderz.com <vishal.thoughtwin@gmail.com>
2022-06-15 16:00:08 +0530

    get data fake api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [data,setData] = useState({})
+  const getData = () => {
+    // console.log("getdata");
+    fetch(' https://jsonplaceholder.typicode.com/todos')
+    .then(res=>res.json())
+
+    .then((data)=>{
+      console.log(data);
+      setData(data)
+    })
+
+    .catch((error)=>{
+      console.log(error)
+    })
+  }
+  useEffect(()=>{
+    //  getData()
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+
+      <button className='btn btn-primary sm w-25 offset-4' onClick={getData}>Get Data</button>
+      <br />
+      <br />
+      <br />
+      
+      <table className=''>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>title</th>
+            <th>userId</th>
+            <th>completed</th>
+          </tr>
+        </thead>
+        <tbody >
+          {
+            data.length > 0 &&
+            data.map((cv)=>{
+              console.log(cv.completed);
+              
+              return (
+                <tr key={cv.id}>
+                  <td>{cv.id}</td>
+                  <td>{cv.title}</td>
+                  <td>{cv.userId}</td>
+                  <td>{cv.completed}</td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
