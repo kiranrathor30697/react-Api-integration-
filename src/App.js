c259bf68d0b0cc8171b9652eb552db69d9f6d3ae
nbvishal@noborderz.com <vishal.thoughtwin@gmail.com>
2022-06-15 17:37:44 +0530

    get data

--- src/App.js
+++ src/App.js
@@ -1,62 +1,15 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Get_api from './components/rfc/Get_Api/Get_api';
 
-const App = () => {
-  const [data,setData] = useState({})
-  const getData = () => {
-    // console.log("getdata");
-    fetch(' https://jsonplaceholder.typicode.com/todos')
-    .then(res=>res.json())
-
-    .then((data)=>{
-      console.log(data);
-      setData(data)
-    })
-
-    .catch((error)=>{
-      console.log(error)
-    })
-  }
-  useEffect(()=>{
-    //  getData()
-  },[])
+export default function App() {
   return (
-    <div className="App App-header">
-
-      <button className='btn btn-primary sm w-25 offset-4' onClick={getData}>Get Data</button>
-      <br />
-      <br />
-      <br />
-      
-      <table className=''>
-        <thead>
-          <tr>
-            <th>id</th>
-            <th>title</th>
-            <th>userId</th>
-            <th>completed</th>
-          </tr>
-        </thead>
-        <tbody >
-          {
-            data.length > 0 &&
-            data.map((cv)=>{
-              console.log(cv.completed);
-              
-              return (
-                <tr key={cv.id}>
-                  <td>{cv.id}</td>
-                  <td>{cv.title}</td>
-                  <td>{cv.userId}</td>
-                  <td>{cv.completed}</td>
-                </tr>
-              )
-            })
-          }
-        </tbody>
-      </table>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Get_api />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/rfc/Get_Api/Get_api.js
@@ -0,0 +1,75 @@
+import { useEffect, useId, useState } from 'react';
+import '../../../App.css';
+
+const Get_api = () => {
+
+  const [data,setData] = useState({});
+
+  const id = useId(data);
+
+  const getData = () => {
+    // console.log("getdata");
+    fetch(' https://jsonplaceholder.typicode.com/todos')
+    .then(res=>res.json())
+
+    .then((data)=>{
+      console.log(data);
+      setData(data)
+    })
+
+    .catch((error)=>{
+      console.log(error)
+    })
+  }
+
+  let view_data = (e) => {
+    // console.log("view");
+
+    console.log(e.currentTarget.id)
+
+  }
+  return (
+    <div className="App App-header">
+
+      <button className='btn btn-primary sm w-25 offset-4' id='id_view' onClick={getData}>Get Data</button>
+      <br />
+      <br />
+      <br />
+      
+      <table className=''>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>title</th>
+            <th>userId</th>
+            <th>completed</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody >
+          {
+            data.length > 0 &&
+            data.map((cv)=>{
+              // console.log(cv.completed);
+              return (
+                <tr key={cv.id}>
+                  <td>{cv.id}</td>
+                  <td>{cv.title}</td>
+                  <td>{cv.userId}</td>
+                  <td>{cv.completed.toString()}</td>
+                  <td>
+                    {
+                      <button className='btn btn-success sm' id="btn_view" value={cv.id} onClick={view_data}>view</button>
+                    }
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default  Get_api;
